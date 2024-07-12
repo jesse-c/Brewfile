2f643fce4ba88b38e9b87f55e5e4c3347fd23ddd
Jesse Claven <jesse.claven@me.com>
2024-07-12 16:57:48 +0100

    refactor(api): Separate string presentation
    This is a step towards better testing.

--- apps/api/spec/lib/brewer/brewer_spec.rb
+++ apps/api/spec/lib/brewer/brewer_spec.rb
@@ -8,25 +8,35 @@ describe Brewer do
     @brewer = Brewer.new
   end
 
+  describe 'when present' do
+    it 'returns nothing' do
+      _(@brewer.present(%w[])).must_equal ""
+    end
+
+    it 'returns something' do
+      _(@brewer.present(%w[Dev-Go Neovim])).must_equal "Dev-Go\nNeovim"
+    end
+  end
+
   describe 'when searching' do
     it 'finds nothing' do
-      _(@brewer.search(%w[z])).must_equal ''
+      _(@brewer.search(%w[z])).must_equal []
     end
 
     it 'finds something' do
-      _(@brewer.search(%w[v Privacy])).must_equal %w[Dev-Go Dev-HTTP Neovim Privacy Vim].join("\n")
+      _(@brewer.search(%w[v Privacy])).must_equal %w[Dev-Go Dev-HTTP Neovim Privacy Vim]
     end
   end
 
   describe 'when listing' do
     it 'lists all brewfiles' do
-      _(@brewer.list).must_equal %w[Core DNS Dev-Go Dev-HTTP Neovim Privacy Python Vim].join("\n")
+      _(@brewer.list).must_equal %w[Core DNS Dev-Go Dev-HTTP Neovim Privacy Python Vim]
     end
   end
 
   describe 'when generating' do
     it 'generates a combined brewfile' do
-      _(@brewer.generate(%w[Neovim Privacy])).must_equal "# brewfile.io\n# Generated from Neovim, Privacy\n\nbrew tap neovim/neovim \nbrew install neovim \nbrew install privoxy"
+      _(@brewer.generate(%w[Neovim Privacy])).must_equal ["# brewfile.io", "# Generated from Neovim, Privacy", "", "brew tap neovim/neovim", "brew install neovim", "brew install privoxy"]
     end
   end
 
